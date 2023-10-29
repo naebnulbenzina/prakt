4537b290da7632901f187871b1be78818b37f59a
Fedor Radionov <Fed200522805@gmail.com>
2023-10-29 20:19:43 +0500

    34

---
+++ pr6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace pr6
+{
+    partial class lab6
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lab6));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(416, 376);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(497, 376);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pct
+            // 
+            this.pct.Image = ((System.Drawing.Image)(resources.GetObject("pct.Image")));
+            this.pct.Location = new System.Drawing.Point(12, 12);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(125, 125);
+            this.pct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct.TabIndex = 2;
+            this.pct.TabStop = false;
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 50;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // lab6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.pct);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Name = "lab6";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ pr6/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr6
+{
+    public partial class lab6 : Form
+    {
+        public lab6()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            if (pct.Location.X > 420)
+            {
+                mov_down();
+                mov_left();
+                mov_up();
+            }
+
+
+            else pct.Left += 20;
+
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (tmr.Enabled)
+            {
+                tmr.Stop();
+                btnStart.Text = "Start";
+            }
+
+            else
+            {
+                tmr.Start();
+                btnStart.Text = "Stop";
+            }
+
+
+        }
+
+        private void mov_left()
+        {
+            while (pct.Location.X > 50)
+            {
+                pct.Left -= 20;
+                Thread.Sleep(50);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void mov_down()
+        {
+            while (pct.Location.Y < 210)
+            {
+                pct.Top += 20;
+                Thread.Sleep(50);
+            }
+        }
+
+        private void mov_up()
+        {
+            while (pct.Location.Y > 15)
+            {
+                pct.Top -= 20;
+                Thread.Sleep(50);
+            }
+        }
+
+     
+    }
+}
