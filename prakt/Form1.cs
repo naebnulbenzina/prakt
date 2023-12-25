7bdbdf995e9a8a09486eb4870c6fbd57d4e4a9d8
Fedor Radionov <Fed200522805@gmail.com>
2023-12-25 12:03:30 +0500

    haahahahahah

--- prakt/Form1.cs
+++ prakt/Form1.cs
@@ -16,5 +16,47 @@ namespace prakt
         {
             InitializeComponent();
         }
+
+        private int numbersymbols(String stroka, char symbol)
+        {
+            int k = 0;
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                if (stroka[i] == symbol)
+                {
+                    k += 1;
+                }
+            }
+            return k;
+        }
+
+        private int numbersymbols(string stroka, int len, string strrr)
+        {
+            int k = 0;
+            int size = stroka.Length - len + 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (stroka.Substring(i, len) == strrr) { k++; i += len - 1; }
+            }
+
+
+            return k;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void knopka_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length == 1)
+            {
+                textBox2.Text = numbersymbols(textBox1.Text, textBox3.Text[0]).ToString();
+            }
+            else
+                textBox2.Text = numbersymbols(textBox1.Text,textBox3.Text.Length,textBox3.Text).ToString();
+        }
     }
 }
